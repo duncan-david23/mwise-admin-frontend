80da06f8269a1e8d33fdb116bc42c52d7b4828a6
D.A.Duncan <duncan.david600@gmail.com>
2025-11-23 10:56:49 +0000

    remove some routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
 import Products from './pages/Products'
-import Orders from './pages/Orders'
-import Offers from './pages/Offers'
-import Newsletter from './pages/Newsletter'
-import Messages from './pages/Messages'
-import Settings from './pages/Settings'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Inventory from './pages/Inventory'
 import Sidebar from './components/Sidebar'
 import { useLocation } from 'react-router-dom'
 import AuthWrapper from './components/AuthWrapper'
@@ -37,44 +30,9 @@ const App = () => {
       <div className="flex">
         <Sidebar />
         <Routes>
-          <Route path="/dashboard" element={
-            <AuthWrapper>
-              <Dashboard />
-            </AuthWrapper>
-          } />
           <Route path="/products" element={
             <AuthWrapper>
             <Products />
-          </AuthWrapper>
-        } />
-        <Route path="/orders" element={
-          <AuthWrapper>
-            <Orders />
-          </AuthWrapper>
-        } />
-        <Route path="/offers" element={
-          <AuthWrapper>
-            <Offers />
-          </AuthWrapper>
-        } />
-        <Route path="/newsletter" element={
-          <AuthWrapper>
-            <Newsletter />
-          </AuthWrapper>
-        } />
-        <Route path="/messages" element={
-          <AuthWrapper>
-            <Messages />
-          </AuthWrapper>
-        } />
-        <Route path="/settings" element={
-          <AuthWrapper>
-            <Settings />
-          </AuthWrapper>
-        } />
-        <Route path="/inventory" element={
-          <AuthWrapper>
-            <Inventory />
           </AuthWrapper>
         } />
         <Route path="/products/add-product" element={
